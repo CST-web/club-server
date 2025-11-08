22041ee2a98c53d07b776dc541d1198f1fedfb5a
Yokai47 <gameryounes555@gmail.com>
2025-11-08 19:11:49 +0100

    feat: update model schemas to match front-end layouts

--- src/models/Member.js
+++ src/models/Member.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const memberSchema = new mongoose.Schema({
   username: { type: String, required: true },
+  cni: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  activities: {
-    type: [
-      {
-        type: String,
-        enum: ["chess", "innovation", "robotic"],
-      },
-    ],
-    validate: {
-      validator: (arr) => arr.length >= 1 && arr.length <= 3,
-      message: "Activities must have between 1 and 3 items.",
-    },
-    default: [],
-  },
-  message: String,
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
 });
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password for login
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return bcrypt.compare(plainPassword, this.password);
+};
 
 export default mongoose.model("Member", memberSchema);
