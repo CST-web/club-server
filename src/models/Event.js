e8195fd328e01ae0e1f7e9dfad693a878285ef4a
younes-alhyan <younesalhyan555@gmail.com>
2025-12-07 13:00:00 +0100

    feat: update model schemas to match front-end layouts

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import { Activity } from "react";
 
 const eventSchema = new mongoose.Schema({
+  activity: {
+    type: String,
+    enum: ["Échecs", "Robotique", "Prix du meilleur TIPE"],
+    required: true,
+  },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  date: Date,
-  image: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  memberLimit: { type: Number, required: true },
+  members: { type: [String], default: [] },
 });
 
 export default mongoose.model("Event", eventSchema);
