8a718a51c5f50453ed2ddedb1820b3560b4a58e3
younes-alhyan <younesalhyan555@gmail.com>
2025-12-11 14:00:00 +0100

    feat(models): set default member verified state to true

--- src/models/Member.js
+++ src/models/Member.js
@@ -7,7 +7,7 @@ const memberSchema = new mongoose.Schema({
   email: { type: String, required: true },
   phone: { type: String, required: true },
   password: { type: String, required: true },
-  verified: { type: Boolean, default: false },
+  verified: { type: Boolean, default: true },
 });
 
 // Hash password before saving
